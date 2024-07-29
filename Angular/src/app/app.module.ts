7d4b2e5a526962593557b03ba5b05e7f89b5fc48
Logesh <logesh644179@gmail.com>
2024-07-29 16:55:58 +0530

    commited

---
+++ Angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LogeshComponent } from './logesh/logesh.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogeshComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
