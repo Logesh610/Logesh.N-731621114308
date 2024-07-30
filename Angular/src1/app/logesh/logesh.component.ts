4ae4a789df20e293d3452f3c4cb7da820fb98805
Logesh <logesh644179@gmail.com>
2024-07-30 18:50:14 +0530

    Angular commited

---
+++ Angular/src1/app/logesh/logesh.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logesh',
+  templateUrl: './logesh.component.html',
+  styleUrl: './logesh.component.css'
+})
+export class LogeshComponent {
+
+}
